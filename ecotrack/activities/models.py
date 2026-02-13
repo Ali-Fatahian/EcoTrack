c76059719cbb86976688e9a4904bb9e9cc86bbff
Ali-Fattahian <ali.fattahian1378@gmail.com>
2026-02-13 22:55:15 +0100

    feat: Create the Category model and run migrations

--- ecotrack/activities/models.py
+++ ecotrack/activities/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=False, unique=True, db_index=True)
+    icon = models.CharField(max_length=255)
+    co2_factor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
