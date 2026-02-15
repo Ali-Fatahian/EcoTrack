5a1d3d039fa603a843a3c752befa6e8b8c58b3d6
Ali-Fattahian <ali.fattahian1378@gmail.com>
2026-02-15 22:35:52 +0100

    feat: Create the UserProfile model

--- ecotrack/users/models.py
+++ ecotrack/users/models.py
@@ -1,5 +1,8 @@
+from decimal import Decimal
+
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.core.validators import MinValueValidator
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -12,7 +15,7 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
 
@@ -24,3 +27,19 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    monthly_goal = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        default=Decimal("150.00"),
+        validators=[MinValueValidator(Decimal("0.00"))],
+        help_text="CO2 limit goal for the month in kg",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Profile for {self.user.username}"
