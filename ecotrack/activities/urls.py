94c63980ddaeaaf5ce0023334b8cca3b91bc6927
Ali-Fattahian <ali.fattahian1378@gmail.com>
2026-02-17 00:54:53 +0100

    feat: Create the dashboard view and connect it to a url path

--- ecotrack/activities/urls.py
+++ ecotrack/activities/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from . import views
+
 app_name = "activities"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.DashboardView.as_view(), name="dashboard"),
+]
--- ecotrack/activities/views.py
+++ ecotrack/activities/views.py
@@ -1,1 +1,57 @@
-# Create your views here.
+import json
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
+from django.utils import timezone
+from django.views.generic import TemplateView
+
+from ecotrack.activities.models import Activity
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "activities/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        user = self.request.user
+
+        activities_this_month = Activity.objects.filter(
+            user=user, date__year=now.year, date__month=now.month
+        )
+        monthly_sum = (
+            activities_this_month.aggregate(monthly_sum=Sum("co2_amount"))[
+                "monthly_sum"
+            ]
+            or 0
+        )  # Just in case it returns None
+
+        category_co2_breakdown = (
+            Activity.objects.filter(
+                user=user, date__year=now.year, date__month=now.month
+            )
+            .values("category__name")
+            .annotate(cat_sum=Sum("co2_amount"))
+            .order_by("-cat_sum")
+        )
+
+        labels = [item["category__name"].title() for item in category_co2_breakdown]
+        data = [float(item["cat_sum"]) for item in category_co2_breakdown]
+
+        monthly_goal = user.profile.monthly_goal
+        if monthly_goal > 0:
+            progress_percentage = (monthly_sum / monthly_goal) * 100
+        else:
+            progress_percentage = 0
+
+        last_five_activities = Activity.objects.filter(user=user)[:5]
+
+        context["monthly_sum"] = monthly_sum
+        context["monthly_goal"] = monthly_goal
+        context["breakdown"] = category_co2_breakdown
+        context["chart_labels"] = json.dumps(labels)
+        context["chart_data"] = json.dumps(data)
+        context["progress_percentage"] = progress_percentage
+        context["recent_activities"] = last_five_activities
+
+        return context
