f13af7bec2a241105fe4b0632d81039b45eab16f
Ali-Fattahian <ali.fattahian1378@gmail.com>
2026-02-17 22:15:12 +0100

    Add filter, search, and list display for the Activity model

--- ecotrack/activities/admin.py
+++ ecotrack/activities/admin.py
@@ -4,4 +4,12 @@ from ecotrack.activities.models import Activity
 from ecotrack.activities.models import Category
 
 admin.site.register(Category)
-admin.site.register(Activity)
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ("date", "user", "category", "quantity", "unit", "co2_amount")
+
+    list_filter = ("category", "date", "unit")
+
+    search_fields = ("description", "user__username", "category__name")
