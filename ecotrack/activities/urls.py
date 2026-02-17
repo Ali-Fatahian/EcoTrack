0df52d85fc0e42e019d38ceaee2b57c0f1520bb1
Ali-Fattahian <ali.fattahian1378@gmail.com>
2026-02-17 15:14:27 +0100

    feat: add the Update, Create, Delete views to urls.py

--- ecotrack/activities/urls.py
+++ ecotrack/activities/urls.py
@@ -7,4 +7,9 @@ app_name = "activities"
 urlpatterns = [
     path("", views.DashboardView.as_view(), name="dashboard"),
     path("activities/", views.ActivityListView.as_view(), name="list"),
+    path("activities/add", views.ActivityCreateView.as_view(), name="create"),
+    path("activities/<int:pk>/edit", views.ActivityUpdateView.as_view(), name="update"),
+    path(
+        "activities/<int:pk>/delete", views.ConfirmDeleteView.as_view(), name="delete"
+    ),
 ]
